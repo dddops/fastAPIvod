383c251ed54628a73c0e7ed7eee83bdc9e27bbbf
dddops <Eopseum0@gmail.com>
2025-10-12 23:08:04 +0900

    4일차 미팅 생성기능

--- app/apis/v1/meeting_router.py
+++ app/apis/v1/meeting_router.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
 from app.dtos.create_meeting_response import CreateMeetingResponse
+from app.services.meeting_service import service_create_meeting
 
 mysql_router = APIRouter(prefix="/v1/meetings", tags=["meeting"])
 
 
-@mysql_router.post("/create-meeting", description="meeting을 생성합니다.")
+@mysql_router.post("", description="meeting을 생성합니다.")
 async def api_create_meeting() -> CreateMeetingResponse:
-    return CreateMeetingResponse(url_code="abc")
+    return CreateMeetingResponse(url_code=(await service_create_meeting()).url_code)
--- app/configs/base_config.py
+++ app/configs/base_config.py
@@ -1,14 +1,21 @@
-class Env(strEnum):
+from enum import StrEnum
+
+from pydantic_settings import BaseSettings
+
+
+class Env(StrEnum):
     LOCAL = "local"
     STAGE = "stage"
     PROD = "prod"
 
 
 class Config(BaseSettings):
-    ENV: Env = Env.local
+    ENV: Env = Env.LOCAL
 
     MYSQL_HOST: str = "localhost"
     MYSQL_PORT: int = 3306
     MYSQL_USER: str = "root"
     MYSQL_PASSWORD: str = "0416"
     MYSQL_DB: str = "FastAPIProject"
+    MYSQL_CONNECT_TIMEOUT: int = 5
+    CONNECTION_POOL_MAXSIZE: int = 30
